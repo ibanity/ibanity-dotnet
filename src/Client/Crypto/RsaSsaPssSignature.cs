40bcf43c9590d4888a62d8ada238ad453c27d4f4
Stéphane de Wit <contact@stephanedewit.be>
2022-03-21 23:43:33 +0100

    Compute RSASSA-PSS signature

--- src/Client/Crypto/RsaSsaPssSignature.cs
+++ src/Client/Crypto/RsaSsaPssSignature.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Ibanity.Apis.Client.Crypto
 {
     public class RsaSsaPssSignature : ISignature
     {
+        private static readonly Encoding Encoding = Encoding.UTF8;
+
+        private readonly X509Certificate2 _certificate;
+
+        public RsaSsaPssSignature(X509Certificate2 certificate) =>
+            _certificate = certificate;
+
         public string Sign(string value)
         {
-            return string.Empty;
+            var bytes = Encoding.GetBytes(value);
+
+            using var privateKey = _certificate.GetRSAPrivateKey();
+            var signature = privateKey.SignData(bytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pss);
+
+            return Convert.ToBase64String(signature);
         }
     }
 
