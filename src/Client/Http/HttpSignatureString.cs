f4a166b6f2813b80d63ce6267c56451e913b093b
Stéphane de Wit <contact@stephanedewit.be>
2022-03-21 18:36:02 +0100

    Get signed headers

--- src/Client/Http/HttpSignatureString.cs
+++ src/Client/Http/HttpSignatureString.cs
@@ -27,10 +27,23 @@ namespace Ibanity.Apis.Client.Http
             }.Union(requestHeaders.
                 Where(kvp => HeaderPattern.IsMatch(kvp.Key)).
                 Select(kvp => $"{kvp.Key.ToLower()}: {kvp.Value}")));
+
+        public IEnumerable<string> GetSignedHeaders(IDictionary<string, string> requestHeaders)
+        {
+            yield return "(request-target)";
+            yield return "host";
+            yield return "digest";
+            yield return "(created)";
+
+            foreach (var key in requestHeaders.Keys)
+                if (HeaderPattern.IsMatch(key))
+                    yield return key.ToLower();
+        }
     }
 
     public interface IHttpSignatureString
     {
         string Compute(string httpMethod, Uri url, IDictionary<string, string> requestHeaders, string digest, DateTimeOffset timestamp);
+        IEnumerable<string> GetSignedHeaders(IDictionary<string, string> requestHeaders);
     }
 }
--- test/Client.Tests/Http/HttpSignatureStringTest.cs
+++ test/Client.Tests/Http/HttpSignatureStringTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ibanity.Apis.Client.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -19,6 +20,8 @@ digest: SHA-512=pX9+OFjSGF4KFWUh8fv1Ihh4PuSb2KnyobO/hr228nkET5vRUhi0Qj2Ai5OcBXtz
 (created): 1548841917
 ibanity-idempotency-key: 61f02718-eeee-46e1-b5eb-e8fd6e799c2d";
 
+        private static readonly string[] ExpectedHeaders = new[] { "(request-target)", "host", "digest", "(created)", "ibanity-idempotency-key" };
+
         [TestMethod]
         public void ComputeReturnsAProperString()
         {
@@ -34,6 +37,14 @@ ibanity-idempotency-key: 61f02718-eeee-46e1-b5eb-e8fd6e799c2d";
                 Timestamp);
 
             Assert.AreEqual(ExpectedString, result);
+        }
+
+        [TestMethod]
+        public void GetSignedHeadersReturnsProperHeaders()
+        {
+            var target = new HttpSignatureString(new Uri("https://api.ibanity.com/"));
+            var result = target.GetSignedHeaders(new Dictionary<string, string> { { "Ibanity-Idempotency-key", IdempotencyKey } });
+            CollectionAssert.AreEqual(ExpectedHeaders, result.ToArray());
         }
     }
 }
