f1c5501b29e053de4e2d7dc2a1a5b0f913680ba9
Stéphane de Wit <contact@stephanedewit.be>
2022-03-21 18:56:04 +0100

    Compose signature header

--- src/Client/Http/HttpSignatureService.cs
+++ src/Client/Http/HttpSignatureService.cs
@@ -1,26 +1,59 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ibanity.Apis.Client.Crypto;
+using Ibanity.Apis.Client.Utils;
 
 namespace Ibanity.Apis.Client.Http
 {
     public class HttpSignatureService : IHttpSignatureService
     {
-        private readonly IDigest _digest;
+        private const string SignatureHeaderAlgorithm = "hs2019";
 
-        public HttpSignatureService(IDigest digest)
+        private readonly IDigest _digest;
+        private readonly ISignature _signature;
+        private readonly IClock _clock;
+        private readonly IHttpSignatureString _signatureString;
+        private readonly string _certificateId;
+
+        public HttpSignatureService(IDigest digest, ISignature signature, IClock clock, IHttpSignatureString signatureString, string certificateId)
         {
             _digest = digest;
+            _signature = signature;
+            _clock = clock;
+            _signatureString = signatureString;
+            _certificateId = certificateId;
         }
 
         public IDictionary<string, string> GetHttpSignatureHeaders(string httpMethod, Uri url, IDictionary<string, string> requestHeaders, string? payload)
         {
+            var now = _clock.Now;
             var digest = _digest.Compute(payload ?? string.Empty);
+
+            var signatureString = _signatureString.Compute(
+                httpMethod,
+                url,
+                requestHeaders,
+                digest,
+                now);
+
+            var signature = _signature.Sign(signatureString);
+
+            var signedHeaders = _signatureString.GetSignedHeaders(requestHeaders);
+
+            var signatureHeaderFields = new[]
+            {
+                $@"keyId=""{_certificateId}""",
+                $@"created={now.ToUnixTimeSeconds()}",
+                $@"algorithm=""{SignatureHeaderAlgorithm}""",
+                $@"headers=""{string.Join(" ", signedHeaders)}""",
+                $@"signature=""{signature}"""
+            };
 
             return new Dictionary<string, string>
             {
                 { "Digest", digest },
-                { "Signature", "" }
+                { "Signature", string.Join(",", signatureHeaderFields) }
             };
         }
     }
--- test/Client.Tests/Http/HttpSignatureServiceTest.cs
+++ test/Client.Tests/Http/HttpSignatureServiceTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Ibanity.Apis.Client.Crypto;
 using Ibanity.Apis.Client.Http;
+using Ibanity.Apis.Client.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -10,8 +11,11 @@ namespace Ibanity.Apis.Client.Tests.Http
     [TestClass]
     public class HttpSignatureServiceTest
     {
+        private static readonly DateTimeOffset Now = DateTimeOffset.Parse("2019-01-30T09:51:57.124733Z");
+        private const string CertificateId = "75b5d796-de5c-400a-81ce-e72371b01cbc";
         private const string IdempotencyKey = "61f02718-eeee-46e1-b5eb-e8fd6e799c2d";
         private const string ExpectedDigest = "SHA-512=pX9+OFjSGF4KFWUh8fv1Ihh4PuSb2KnyobO/hr228nkET5vRUhi0Qj2Ai5OcBXtzmzgII18sZiaEH4PoxkYqew==";
+        private const string ExpectedSignature = @"keyId=""75b5d796-de5c-400a-81ce-e72371b01cbc"",created=1548841917,algorithm=""hs2019"",headers=""foo (bar)"",signature=""mock-signature""";
 
         [TestMethod]
         public void GetHttpSignatureHeadersReturnsExactlyTwoHeaders()
@@ -33,6 +37,14 @@ namespace Ibanity.Apis.Client.Tests.Http
             Assert.AreEqual(ExpectedDigest, headers["Digest"]);
         }
 
+        [TestMethod]
+        public void GetHttpSignatureHeadersReturnsProperSignature()
+        {
+            var headers = GetHeaders();
+
+            Assert.AreEqual(ExpectedSignature, headers["Signature"]);
+        }
+
         private IDictionary<string, string> GetHeaders()
         {
             var digest = new Mock<IDigest>();
@@ -40,7 +52,36 @@ namespace Ibanity.Apis.Client.Tests.Http
                 Setup(d => d.Compute(It.IsAny<string>())).
                 Returns(ExpectedDigest);
 
-            var target = new HttpSignatureService(digest.Object);
+            var signature = new Mock<ISignature>();
+            signature.
+                Setup(s => s.Sign(It.IsAny<string>())).
+                Returns("mock-signature");
+
+            var clock = new Mock<IClock>();
+            clock.
+                Setup(c => c.Now).
+                Returns(Now);
+
+            var signatureString = new Mock<IHttpSignatureString>();
+            signatureString.
+                Setup(s => s.Compute(
+                    It.IsAny<string>(),
+                    It.IsAny<Uri>(),
+                    It.IsAny<IDictionary<string, string>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<DateTimeOffset>())).
+                Returns("mock-signature-string");
+
+            signatureString.
+                Setup(s => s.GetSignedHeaders(It.IsAny<IDictionary<string, string>>())).
+                Returns(new[] { "foo", "(bar)" });
+
+            var target = new HttpSignatureService(
+                digest.Object,
+                signature.Object,
+                clock.Object,
+                signatureString.Object,
+                CertificateId);
 
             return target.GetHttpSignatureHeaders(
                 "POST",
