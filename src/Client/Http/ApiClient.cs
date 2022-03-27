62de5366acf0bfe2e965b2f8b0f35a41c944fbd1
Stéphane de Wit <contact@stephanedewit.be>
2022-03-27 18:27:59 +0200

    Remove useless 'using' statements

--- src/Client/Http/ApiClient.cs
+++ src/Client/Http/ApiClient.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Ibanity.Apis.Client.Utils;
