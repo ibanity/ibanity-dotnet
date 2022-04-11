5d61f3fe3664439d28f838ad8fc362eeb62bf192
Stéphane de Wit <contact@stephanedewit.be>
2022-04-12 00:01:07 +0200

    Document product client-related classes

--- src/Client/Products/ProductClient.cs
+++ src/Client/Products/ProductClient.cs
@@ -2,8 +2,17 @@ using Ibanity.Apis.Client.Http;
 
 namespace Ibanity.Apis.Client.Products
 {
+    /// <summary>
+    /// Base product client.
+    /// </summary>
     public abstract class ProductClient : IProductClient
     {
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="apiClient">Generic API client</param>
+        /// <param name="tokenService">Service to generate and refresh access tokens</param>
+        /// <param name="clientAccessTokenService">Service to generate and refresh client access tokens.</param>
         public ProductClient(IApiClient apiClient, ITokenProvider tokenService, IClientAccessTokenProvider clientAccessTokenService)
         {
             ApiClient = apiClient ?? throw new System.ArgumentNullException(nameof(apiClient));
@@ -11,15 +20,34 @@ namespace Ibanity.Apis.Client.Products
             ClientTokenService = clientAccessTokenService ?? throw new System.ArgumentNullException(nameof(clientAccessTokenService));
         }
 
+        /// <inheritdoc />
         public IApiClient ApiClient { get; }
+
+        /// <inheritdoc />
         public ITokenProvider TokenService { get; }
+
+        /// <inheritdoc />
         public IClientAccessTokenProvider ClientTokenService { get; }
     }
 
+    /// <summary>
+    /// Base product client interface.
+    /// </summary>
     public interface IProductClient
     {
+        /// <summary>
+        /// Generic API client.
+        /// </summary>
         IApiClient ApiClient { get; }
+
+        /// <summary>
+        /// Service to generate and refresh access tokens.
+        /// </summary>
         ITokenProvider TokenService { get; }
+
+        /// <summary>
+        /// Service to generate and refresh client access tokens.
+        /// </summary>
         IClientAccessTokenProvider ClientTokenService { get; }
     }
 }
