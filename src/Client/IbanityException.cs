1efb5a5d835d1bc6cf80d28e8b8ed0e0bb1126cc
Stéphane de Wit <contact@stephanedewit.be>
2022-04-11 20:06:42 +0200

    Document error-related classes

--- src/Client/IbanityException.cs
+++ src/Client/IbanityException.cs
@@ -2,13 +2,24 @@ using System;
 
 namespace Ibanity.Apis.Client
 {
+    /// <summary>
+    /// Base exception for all Ibanity errors.
+    /// </summary>
     public class IbanityException : ApplicationException
     {
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="message">Error description</param>
         public IbanityException(string message) : base(message) { }
     }
 
+    /// <summary>
+    /// Something is misconfigured.
+    /// </summary>
     public class IbanityConfigurationException : IbanityException
     {
+        /// <inheritdoc />
         public IbanityConfigurationException(string message) : base(message) { }
     }
 }
