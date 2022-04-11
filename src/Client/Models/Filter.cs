a8379fec236f5d8deb9278ec527582d17dee245d
Stéphane de Wit <contact@stephanedewit.be>
2022-04-11 19:29:47 +0200

    Document filters-related classes

--- src/Client/Models/Filter.cs
+++ src/Client/Models/Filter.cs
@@ -1,11 +1,20 @@
 namespace Ibanity.Apis.Client.Models
 {
+    /// <summary>
+    /// Filter results when using <c>List</c> methods.
+    /// </summary>
     public class Filter
     {
         private readonly string _field;
         private readonly FilterOperator _operator;
         private readonly string _value;
 
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="field">Resource field to check</param>
+        /// <param name="operator">Comparison type</param>
+        /// <param name="value">Value to check for</param>
         public Filter(string field, FilterOperator @operator, string value)
         {
             if (string.IsNullOrWhiteSpace(field))
@@ -19,15 +28,34 @@ namespace Ibanity.Apis.Client.Models
             _value = value;
         }
 
+        /// <summary>
+        /// String representation of the filter.
+        /// </summary>
+        /// <returns>Filter to be used in query string</returns>
         public override string ToString() =>
             $"filter[{_field}][{_operator.ToString("g").ToLower()}]={_value}";
     }
 
+    /// <summary>
+    /// Comparison types.
+    /// </summary>
     public enum FilterOperator
     {
+        /// <summary>
+        /// Equals.
+        /// </summary>
         Eq,
+        /// <summary>
+        /// Fuzzy match.
+        /// </summary>
         Like,
+        /// <summary>
+        /// Contains.
+        /// </summary>
         Contains,
+        /// <summary>
+        /// In.
+        /// </summary>
         In
     }
 }
