c56e00727a904cd3ba0dbb578c5dd97548d2428e
Stéphane de Wit <contact@stephanedewit.be>
2022-04-14 20:57:03 +0200

    Create empty 'products' page

---
+++ samples/webapp/Pages/Products.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages;
+
+public class ProductsModel : PageModel
+{
+    private readonly ILogger<ProductsModel> _logger;
+
+    public ProductsModel(ILogger<ProductsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
