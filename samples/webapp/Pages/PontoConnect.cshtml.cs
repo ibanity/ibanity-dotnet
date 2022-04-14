8a4f4bd0eee6a4962d19719b9dc7b2b4f7953e7a
Stéphane de Wit <contact@stephanedewit.be>
2022-04-15 00:19:53 +0200

    Add empty Ponto Connect page

---
+++ samples/webapp/Pages/PontoConnect.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages;
+
+public class PontoConnectModel : PageModel
+{
+    private readonly ILogger<PontoConnectModel> _logger;
+
+    public PontoConnectModel(ILogger<PontoConnectModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        if (!HttpContext.Session.Keys.Contains("PontoConnectToken"))
+            return Redirect("/Products");
+
+        return Page();
+    }
+}
