952eb984c98656762a2b0b8ad05c05dc590bf106
Stéphane de Wit <contact@stephanedewit.be>
2022-04-19 22:13:25 +0200

    Add 'invalid signature' exception

---
+++ src/Client/Webhooks/InvalidSignatureException.cs
@@ -0,0 +1,16 @@
+namespace Ibanity.Apis.Client.Webhooks
+{
+    /// <summary>
+    /// Invalid webhook signature.
+    /// </summary>
+    public class InvalidSignatureException : IbanityException
+    {
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="message">Error description</param>
+        public InvalidSignatureException(string message) : base(message)
+        {
+        }
+    }
+}
