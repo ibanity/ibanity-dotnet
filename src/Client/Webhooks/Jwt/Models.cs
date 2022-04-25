78477d2f12e8c7bbe53a397624670b22c934c9fc
Stéphane de Wit <contact@stephanedewit.be>
2022-04-26 01:38:41 +0200

    Parse JWT timestamps

--- src/Client/Webhooks/Jwt/Models.cs
+++ src/Client/Webhooks/Jwt/Models.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.Serialization;
 
 namespace Ibanity.Apis.Client.Webhooks.Jwt
@@ -28,16 +29,26 @@ namespace Ibanity.Apis.Client.Webhooks.Jwt
     public class Payload
     {
         /// <summary>
-        /// Expiration time
+        /// Expiration time (seconds since Unix epoch).
         /// </summary>
         [DataMember(Name = "exp", EmitDefaultValue = false)]
-        public string Expiration { get; set; }
+        public long ExpirationEpoch { get; set; }
+
+        /// <summary>
+        /// Expiration time.
+        /// </summary>
+        public DateTimeOffset Expiration => DateTimeOffset.FromUnixTimeSeconds(ExpirationEpoch);
+
+        /// <summary>
+        /// Issued at (seconds since Unix epoch).
+        /// </summary>
+        [DataMember(Name = "iat", EmitDefaultValue = false)]
+        public long IssuedAtEpoch { get; set; }
 
         /// <summary>
         /// Issued at.
         /// </summary>
-        [DataMember(Name = "iat", EmitDefaultValue = false)]
-        public string IssuedAt { get; set; }
+        public DateTimeOffset IssuedAt => DateTimeOffset.FromUnixTimeSeconds(IssuedAtEpoch);
 
         /// <summary>
         /// Who created and signed this token.
