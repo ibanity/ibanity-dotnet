9317959d6dd10c1b9ba8051c3f8a6e9d1387a21a
Stéphane de Wit <contact@stephanedewit.be>
2022-04-26 00:58:57 +0200

    Add JWKS cache

--- src/Client/Webhooks/Jwt/JwksServiceCachingDecorator.cs
+++ src/Client/Webhooks/Jwt/JwksServiceCachingDecorator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +13,14 @@ namespace Ibanity.Apis.Client.Webhooks.Jwt
     /// </summary>
     public class JwksServiceCachingDecorator : IJwksService
     {
+        private readonly ConcurrentDictionary<string, CacheEntry> _cache = new ConcurrentDictionary<string, CacheEntry>();
+
+        private struct CacheEntry
+        {
+            public DateTimeOffset InsertedAt { get; set; }
+            public RSA Value { get; set; }
+        }
+
         private readonly IJwksService _underlyingInstance;
         private readonly IClock _clock;
         private readonly TimeSpan _ttl;
@@ -29,9 +39,29 @@ namespace Ibanity.Apis.Client.Webhooks.Jwt
         }
 
         /// <inheritdoc />
-        public Task<RSA> GetPublicKey(string keyId, CancellationToken? cancellationToken = null)
+        public async Task<RSA> GetPublicKey(string keyId, CancellationToken? cancellationToken = null)
         {
-            return _underlyingInstance.GetPublicKey(keyId, cancellationToken);
+            var threshold = _clock.Now - _ttl;
+            var toRemove = _cache.
+                Where(kvp => kvp.Value.InsertedAt < threshold).
+                Select(kvp => kvp.Key).
+                ToArray();
+
+            foreach (var id in toRemove)
+                _cache.TryRemove(id, out _);
+
+            if (_cache.TryGetValue(keyId, out var entry))
+                return entry.Value;
+
+            entry = new CacheEntry
+            {
+                InsertedAt = _clock.Now,
+                Value = await _underlyingInstance.GetPublicKey(keyId, cancellationToken)
+            };
+
+            _cache[keyId] = entry;
+
+            return entry.Value;
         }
     }
 }
