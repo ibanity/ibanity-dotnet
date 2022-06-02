d13791672312d3282eed5d7935c7ff08c95156c7
Stéphane de Wit <contact@stephanedewit.be>
2022-06-02 23:28:58 +0200

    List balances

---
+++ src/Client/Products/IsabelConnect/Balances.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Ibanity.Apis.Client.Http;
+using Ibanity.Apis.Client.Products.IsabelConnect.Models;
+using Ibanity.Apis.Client.Utils;
+
+namespace Ibanity.Apis.Client.Products.IsabelConnect
+{
+    /// <summary>
+    /// This is an object representing a balance related to a customer's account.
+    /// </summary>
+    public class Balances : ResourceWithParentClient<BalanceWithFakeId, object, object, object, string>, IBalances
+    {
+        private const string ParentEntityName = "accounts";
+        private const string EntityName = "balances";
+
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="apiClient">Generic API client</param>
+        /// <param name="accessTokenProvider">Service to refresh access tokens</param>
+        /// <param name="urlPrefix">Beginning of URIs, composed by Ibanity API endpoint, followed by product name</param>
+        public Balances(IApiClient apiClient, IAccessTokenProvider accessTokenProvider, string urlPrefix) :
+            base(apiClient, accessTokenProvider, urlPrefix, new[] { ParentEntityName, EntityName })
+        { }
+
+        /// <inheritdoc />
+        public async Task<IsabelCollection<Balance>> List(Token token, string accountId, DateTimeOffset? from = null, DateTimeOffset? to = null, long? pageOffset = null, int? pageSize = null, CancellationToken? cancellationToken = null)
+        {
+            var result = await InternalOffsetBasedList(token, new[] { accountId }, null, pageOffset, pageSize, cancellationToken);
+
+            return new IsabelCollection<Balance>
+            {
+                Offset = result.Offset,
+                Total = result.Total,
+                Items = result.Items.Select(b => new Balance(b)).ToList(),
+                ContinuationToken = result.ContinuationToken
+            };
+        }
+
+        /// <inheritdoc />
+        protected override string ParseId(string id) => id;
+    }
+
+    /// <summary>
+    /// This is an object representing a balance related to a customer's account.
+    /// </summary>
+    public interface IBalances
+    {
+        /// <summary>
+        /// List Accounts Balances
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="accountId">Customer's account ID</param>
+        /// <param name="from">Start of the period scope. Mandatory if to is present. If not provided, the most recent balance of each type is returned.</param>
+        /// <param name="to">End of the period scope. Must be equal to or later than from value. Defaults to today's date.</param>
+        /// <param name="pageOffset">Defines the start position of the results by giving the number of records to be skipped</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of balance resources</returns>
+        /// <remarks>The range may contain maximum 31 calendar days (e.g. 2020-10-01-2020-10-31).</remarks>
+        Task<IsabelCollection<Balance>> List(Token token, string accountId, DateTimeOffset? from = null, DateTimeOffset? to = null, long? pageOffset = null, int? pageSize = null, CancellationToken? cancellationToken = null);
+    }
+}
--- src/Client/Products/IsabelConnect/Models/Balance.cs
+++ src/Client/Products/IsabelConnect/Models/Balance.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.Serialization;
+using Ibanity.Apis.Client.Utils;
 
 namespace Ibanity.Apis.Client.Products.IsabelConnect.Models
 {
@@ -9,6 +10,26 @@ namespace Ibanity.Apis.Client.Products.IsabelConnect.Models
     [DataContract]
     public class Balance
     {
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        public Balance() { }
+
+        /// <summary>
+        /// Build a new instance.
+        /// </summary>
+        /// <param name="from">Source instance</param>
+        /// <remarks>Copy constructor.</remarks>
+        public Balance(Balance from)
+        {
+            if (from is null)
+                throw new ArgumentNullException(nameof(from));
+
+            Amount = from.Amount;
+            Datetime = from.Datetime;
+            Subtype = from.Subtype;
+        }
+
         /// <summary>
         /// Amount of the balance. Can be positive or negative
         /// </summary>
@@ -29,5 +50,16 @@ namespace Ibanity.Apis.Client.Products.IsabelConnect.Models
         /// <value>Type of balance. The possible values are &lt;code&gt;CLBD&lt;/code&gt; (Closing Balance), &lt;code&gt;CLAV&lt;/code&gt; (Closing Available Balance), &lt;code&gt;ITBD&lt;/code&gt; (Intraday Balance), &lt;code&gt;ITAV&lt;/code&gt; (Intraday Available Balance), or &lt;code&gt;INFO&lt;/code&gt; if derived from the previous statement.</value>
         [DataMember(Name = "subtype", EmitDefaultValue = false)]
         public string Subtype { get; set; }
+    }
+
+    /// <summary>
+    /// Technical-only object, prefer <see cref="Balance" /> usage.
+    /// </summary>
+    public class BalanceWithFakeId : Balance, IIdentified<string>
+    {
+        /// <summary>
+        /// Fake field.
+        /// </summary>
+        public string Id { get; set; }
     }
 }
