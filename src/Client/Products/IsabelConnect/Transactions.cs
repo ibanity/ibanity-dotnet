9319f8dd95d18e4559bb8fe98767552273cc0aa1
Stéphane de Wit <contact@stephanedewit.be>
2022-06-04 00:22:08 +0200

    List transactions

--- src/Client/Products/IsabelConnect/Transactions.cs
+++ src/Client/Products/IsabelConnect/Transactions.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
 using Ibanity.Apis.Client.Http;
 using Ibanity.Apis.Client.Products.IsabelConnect.Models;
+using Ibanity.Apis.Client.Utils;
 
 namespace Ibanity.Apis.Client.Products.IsabelConnect
 {
@@ -24,6 +29,20 @@ namespace Ibanity.Apis.Client.Products.IsabelConnect
         { }
 
         /// <inheritdoc />
+        public Task<IsabelCollection<Transaction>> List(Token token, string accountId, DateTimeOffset? from = null, DateTimeOffset? to = null, long? pageOffset = null, int? pageSize = null, CancellationToken? cancellationToken = null)
+        {
+            var timespanParameters = new List<(string, string)>();
+
+            if (from.HasValue)
+                timespanParameters.Add(("from", from.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)));
+
+            if (to.HasValue)
+                timespanParameters.Add(("to", to.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)));
+
+            return InternalOffsetBasedList(token, new[] { accountId }, null, timespanParameters, pageOffset, pageSize, cancellationToken);
+        }
+
+        /// <inheritdoc />
         protected override Guid ParseId(string id) => Guid.Parse(id);
     }
 
@@ -33,5 +52,17 @@ namespace Ibanity.Apis.Client.Products.IsabelConnect
     /// </summary>
     public interface ITransactions
     {
+        /// <summary>
+        /// List Transactions
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="accountId">Customer's account ID</param>
+        /// <param name="from">Start of the period scope, in the ISO8601 format YYYY-MM-DD. Mandatory if to is present. Defaults to today's date. Must be within 365 days of today's date and within 200 days of the to date.</param>
+        /// <param name="to">End of the period scope, in the ISO8601 format YYYY-MM-DD. Must be equal to or later than from value. Defaults to today's date.</param>
+        /// <param name="pageOffset">Defines the start position of the results by giving the number of records to be skipped</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of transaction resources</returns>
+        Task<IsabelCollection<Transaction>> List(Token token, string accountId, DateTimeOffset? from = null, DateTimeOffset? to = null, long? pageOffset = null, int? pageSize = null, CancellationToken? cancellationToken = null);
     }
 }
