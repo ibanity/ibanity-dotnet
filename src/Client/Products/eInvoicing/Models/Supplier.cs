17d0fb9b149d1e54d55ccf957f64ceb27a841835
Stéphane de Wit <contact@stephanedewit.be>
2022-07-04 23:42:05 +0200

    Deserialize supplier IBANs and names

--- src/Client/Products/eInvoicing/Models/Supplier.cs
+++ src/Client/Products/eInvoicing/Models/Supplier.cs
@@ -64,14 +64,14 @@ namespace Ibanity.Apis.Client.Products.eInvoicing.Models
         /// </summary>
         /// <value>An array of objects representing the IBANs of the supplier.&lt;ul&gt;&lt;li&gt;&lt;code&gt;id&lt;/code&gt;An uuid of the IBAN number of the supplier.&lt;/li&gt;&lt;li&gt;&lt;code&gt;value&lt;/code&gt;The supplier IBAN of the supplier.&lt;/li&gt;&lt;/ul&gt;</value>
         [DataMember(Name = "ibans", EmitDefaultValue = false)]
-        public List<Object> Ibans { get; set; }
+        public List<SupplierIban> Ibans { get; set; }
 
         /// <summary>
         /// An array of objects representing the company names of the supplier.&lt;ul&gt;&lt;li&gt;&lt;code&gt;id&lt;/code&gt;An uuid of the company name of the supplier.&lt;/li&gt;&lt;li&gt;&lt;code&gt;value&lt;/code&gt;The company name of the supplier.&lt;/li&gt;&lt;/ul&gt;
         /// </summary>
         /// <value>An array of objects representing the company names of the supplier.&lt;ul&gt;&lt;li&gt;&lt;code&gt;id&lt;/code&gt;An uuid of the company name of the supplier.&lt;/li&gt;&lt;li&gt;&lt;code&gt;value&lt;/code&gt;The company name of the supplier.&lt;/li&gt;&lt;/ul&gt;</value>
         [DataMember(Name = "names", EmitDefaultValue = false)]
-        public List<Object> Names { get; set; }
+        public List<SupplierName> Names { get; set; }
 
         /// <summary>
         /// The status of the onboarding process of this supplier, for more information see &lt;a href&#x3D;\&quot;https://documentation.ibanity.com/einvoicing/products#suppliers\&quot;&gt;Suppliers&lt;/a&gt;.&lt;ul&gt;&lt;li&gt;&lt;code&gt;created&lt;/code&gt;The supplier was successfully created at CodaBox.&lt;/li&gt;&lt;li&gt;&lt;code&gt;approved&lt;/code&gt;The supplier information was validated by CodaBox and the supplier will be onboarded on the einvoicing network(s).&lt;/li&gt;&lt;li&gt;&lt;code&gt;rejected&lt;/code&gt;There was a problem with the supplier information and CodaBox could not accept the supplier.&lt;/li&gt;&lt;li&gt;&lt;code&gt;onboarded&lt;/code&gt;The supplier is now fully onboarded, you can now start sending documents for this supplier.&lt;/li&gt;&lt;li&gt;&lt;code&gt;offboarded&lt;/code&gt;The supplier was successfully offboarded, you can no longer send documents for this supplier.&lt;/li&gt;&lt;/ul&gt;
@@ -128,5 +128,43 @@ namespace Ibanity.Apis.Client.Products.eInvoicing.Models
         /// <value>The zipcode of the city where the supplier is located.</value>
         [DataMember(Name = "zip", EmitDefaultValue = false)]
         public string Zip { get; set; }
+    }
+
+    /// <summary>
+    /// IBAN of the supplier.
+    /// </summary>
+    [DataContract]
+    public class SupplierIban
+    {
+        /// <summary>
+        /// An uuid of the IBAN number of the supplier.
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The supplier IBAN of the supplier.
+        /// </summary>
+        [DataMember(Name = "value", EmitDefaultValue = false)]
+        public string Value { get; set; }
+    }
+
+    /// <summary>
+    /// Name of the supplier.
+    /// </summary>
+    [DataContract]
+    public class SupplierName
+    {
+        /// <summary>
+        /// An uuid of the company name of the supplier.
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The company name of the supplier.
+        /// </summary>
+        [DataMember(Name = "value", EmitDefaultValue = false)]
+        public string Value { get; set; }
     }
 }
