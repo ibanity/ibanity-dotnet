3a45a0a1114a770b9220d23f1319e69cba5ce3d5
Stéphane de Wit <contact@stephanedewit.be>
2022-08-30 18:25:34 +0200

    Use automatic properties

--- src/Client/Products/eInvoicing/Models/ZoomitDocument.cs
+++ src/Client/Products/eInvoicing/Models/ZoomitDocument.cs
@@ -22,12 +22,10 @@ namespace Ibanity.Apis.Client.Products.eInvoicing.Models
         /// When this zoomit document was created. Formatted according to &lt;a href&#x3D;&#39;https://en.wikipedia.org/wiki/ISO_8601&#39;&gt;ISO8601&lt;/a&gt; spec
         /// </summary>
         /// <value>When this zoomit document was created. Formatted according to &lt;a href&#x3D;&#39;https://en.wikipedia.org/wiki/ISO_8601&#39;&gt;ISO8601&lt;/a&gt; spec</value>
-        public DateTimeOffset CreatedAt
-        {
-            get => DateTimeOffset.TryParse(CreatedAtString, out var createdDate)
+        public DateTimeOffset CreatedAt =>
+            DateTimeOffset.TryParse(CreatedAtString, out var createdDate)
                 ? createdDate
                 : DateTimeOffset.ParseExact(CreatedAtString.Replace(" 00:00", "+00:00"), "yyyy-MM-ddTHH:mm:ss.fffffffzzz", CultureInfo.InvariantCulture);
-        }
 
         /// <summary>
         /// &lt;p&gt;The status of the document.&lt;/p&gt;&lt;p&gt;Possible values&lt;/p&gt;&lt;ul&gt;&lt;li&gt;&lt;code&gt;created&lt;/code&gt; The document was successfully received by CodaBox and will be processed.&lt;/li&gt;&lt;li&gt;&lt;code&gt;sending&lt;/code&gt; The document is valid and will be sent to the customer.&lt;/li&gt;&lt;li&gt;&lt;code&gt;sent&lt;/code&gt; The document is available for the customer in Zoomit. In this case you receive a transactionId.&lt;/li&gt;&lt;li&gt;&lt;code&gt;invalid&lt;/code&gt; The document is not valid, you will receive an error object containing a code and a message explaining what went wrong (see below).&lt;/li&gt;&lt;li&gt;&lt;code&gt;send-error&lt;/code&gt; The document could not be sent to the customer, you will receive an error object containing a code and a message explaining what went wrong (see below).    &lt;/li&gt;&lt;/ul&gt;&lt;p&gt;In case of an unspecified error or an issue with the receiving access point, you can try to resend the the document. CodaBox will not automatically resend the the document, send-error is a final state of a document.&lt;/p&gt;
