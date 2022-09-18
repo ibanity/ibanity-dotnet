bc24e1d1dfe49f5ba6572d27949f49cb83dd4d95
Stéphane de Wit <contact@stephanedewit.be>
2022-09-18 18:24:35 +0200

    Get bank account statement PDF

--- src/Client/Products/CodaboxConnect/BankAccountStatements.cs
+++ src/Client/Products/CodaboxConnect/BankAccountStatements.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Ibanity.Apis.Client.Http;
@@ -27,6 +28,10 @@ namespace Ibanity.Apis.Client.Products.CodaboxConnect
             InternalGet(token, new[] { accountingOfficeId.ToString(), clientId }, id, cancellationToken);
 
         /// <inheritdoc />
+        public Task GetPdf(ClientAccessToken token, Guid accountingOfficeId, string clientId, Guid id, Stream target, CancellationToken? cancellationToken = null) =>
+            InternalGetToStream(token, new[] { accountingOfficeId.ToString(), clientId }, id, "application/pdf", target, cancellationToken);
+
+        /// <inheritdoc />
         protected override Guid ParseId(string id) => Guid.Parse(id);
     }
 
@@ -45,5 +50,17 @@ namespace Ibanity.Apis.Client.Products.CodaboxConnect
         /// <param name="cancellationToken">Allow to cancel a long-running task</param>
         /// <returns>Returns a Bank Account Statement resource.</returns>
         Task<BankAccountStatement> Get(ClientAccessToken token, Guid accountingOfficeId, string clientId, Guid id, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// Get Bank Account Statement PDF
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="accountingOfficeId">Accounting office identifier</param>
+        /// <param name="clientId">Bank account statement's owner</param>
+        /// <param name="id">Bank Account Statement ID</param>
+        /// <param name="target">Destination stream where the PDF document will be written</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>Returns a PDF representation of the bank account statement.</returns>
+        Task GetPdf(ClientAccessToken token, Guid accountingOfficeId, string clientId, Guid id, Stream target, CancellationToken? cancellationToken = null);
     }
 }
