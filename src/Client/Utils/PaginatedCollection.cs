f8c658553dabe48ec01225b4b89c5f9f1205c4c5
Stéphane de Wit <contact@stephanedewit.be>
2022-10-31 22:28:41 +0100

    Add page-based XS2A pagination

--- src/Client/Utils/PaginatedCollection.cs
+++ src/Client/Utils/PaginatedCollection.cs
@@ -109,6 +109,56 @@ namespace Ibanity.Apis.Client.Utils
     }
 
     /// <summary>
+    /// <para>List of resources.</para>
+    /// <para>Contains a token to get the next page.</para>
+    /// </summary>
+    /// <typeparam name="T">Resource type.</typeparam>
+#pragma warning disable CA1711 // Keep 'Collection' name
+    public class PageBasedXS2ACollection<T> : PaginatedCollection<T>
+#pragma warning restore CA1711
+    {
+        /// <summary>
+        /// Start position of the results by giving the page index.
+        /// </summary>
+        public long? Number { get; set; }
+
+        /// <summary>
+        /// Number (1-2000) of document resources returned.
+        /// </summary>
+        public int? Size { get; set; }
+
+        /// <summary>
+        /// Number of total pages in the requested scope.
+        /// </summary>
+        public long? TotalPages { get; set; }
+
+        /// <summary>
+        /// Number of total resources in the requested scope.
+        /// </summary>
+        public long? TotalEntries { get; set; }
+
+        /// <summary>
+        /// Link to the first page.
+        /// </summary>
+        public string FirstLink { get; set; }
+
+        /// <summary>
+        /// Link to the previous page.
+        /// </summary>
+        public string PreviousLink { get; set; }
+
+        /// <summary>
+        /// Link to the next page.
+        /// </summary>
+        public string NextLink { get; set; }
+
+        /// <summary>
+        /// Link to the next page.
+        /// </summary>
+        public string LastLink { get; set; }
+    }
+
+    /// <summary>
     /// Token allowing to fetch the next page of a <see cref="IbanityCollection&lt;T&gt;" /> or a <see cref="IsabelCollection&lt;T&gt;" />.
     /// </summary>
     public class ContinuationToken
