48fa3a7332ddcb8de8cb3248d5de1c405d0ff34c
Stéphane de Wit <contact@stephanedewit.be>
2022-10-31 23:53:24 +0100

    List financial institutions

--- src/Client/Products/XS2A/FinancialInstitutions.cs
+++ src/Client/Products/XS2A/FinancialInstitutions.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Ibanity.Apis.Client.Http;
 using Ibanity.Apis.Client.JsonApi;
+using Ibanity.Apis.Client.Models;
 using Ibanity.Apis.Client.Products.XS2A.Models;
+using Ibanity.Apis.Client.Utils;
 
 namespace Ibanity.Apis.Client.Products.XS2A
 {
@@ -20,6 +26,40 @@ namespace Ibanity.Apis.Client.Products.XS2A
         { }
 
         /// <inheritdoc />
+        public Task<IbanityCollection<FinancialInstitutionResponse>> List(IEnumerable<Filter> filters = null, int? pageSize = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null) =>
+            InternalCursorBasedList(
+                null,
+                filters,
+                pageSize,
+                pageBefore,
+                pageAfter,
+                cancellationToken);
+
+        /// <inheritdoc />
+        public Task<PageBasedXS2ACollection<FinancialInstitutionResponse>> List(IEnumerable<Filter> filters = null, long? pageNumber = null, int? pageSize = null, CancellationToken? cancellationToken = null) =>
+            InternalXs2aPageBasedList(null, filters, null, pageNumber, pageSize, cancellationToken);
+
+        /// <inheritdoc />
+        public Task<IbanityCollection<FinancialInstitutionResponse>> ListForCustomer(CustomerAccessToken token, IEnumerable<Filter> filters = null, int? pageSize = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null) =>
+            InternalCursorBasedList(
+                token ?? throw new ArgumentNullException(nameof(token)),
+                filters,
+                pageSize,
+                pageBefore,
+                pageAfter,
+                cancellationToken);
+
+        /// <inheritdoc />
+        public Task<PageBasedXS2ACollection<FinancialInstitutionResponse>> ListForCustomer(CustomerAccessToken token, IEnumerable<Filter> filters = null, long? pageNumber = null, int? pageSize = null, CancellationToken? cancellationToken = null) =>
+            InternalXs2aPageBasedList(
+                token ?? throw new ArgumentNullException(nameof(token)),
+                filters,
+                null,
+                pageNumber,
+                pageSize,
+                cancellationToken);
+
+        /// <inheritdoc />
         protected override FinancialInstitutionResponse Map(Data<FinancialInstitutionResponse, object, object, FinancialInstitutionLinks> data)
         {
             var result = base.Map(data);
@@ -36,5 +76,48 @@ namespace Ibanity.Apis.Client.Products.XS2A
     /// <remarks>You can manage fake financial institutions in the sandbox using the create, update, and delete methods. Obviously, these endpoints will not work for real, live financial institutions.</remarks>
     public interface IFinancialInstitutions
     {
+        /// <summary>
+        /// List Financial Institutions
+        /// </summary>
+        /// <param name="filters">Attributes to be filtered from the results</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="pageBefore">Cursor that specifies the first resource of the next page</param>
+        /// <param name="pageAfter">Cursor that specifies the last resource of the previous page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of financial institution resources</returns>
+        Task<IbanityCollection<FinancialInstitutionResponse>> List(IEnumerable<Filter> filters = null, int? pageSize = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// List Financial Institutions
+        /// </summary>
+        /// <param name="filters">Attributes to be filtered from the results</param>
+        /// <param name="pageNumber">Cursor that specifies the last resource of the previous page</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of financial institution resources</returns>
+        Task<PageBasedXS2ACollection<FinancialInstitutionResponse>> List(IEnumerable<Filter> filters = null, long? pageNumber = null, int? pageSize = null, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// List Financial Institutions for a customer
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="filters">Attributes to be filtered from the results</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="pageBefore">Cursor that specifies the first resource of the next page</param>
+        /// <param name="pageAfter">Cursor that specifies the last resource of the previous page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of financial institution resources</returns>
+        Task<IbanityCollection<FinancialInstitutionResponse>> ListForCustomer(CustomerAccessToken token, IEnumerable<Filter> filters = null, int? pageSize = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// List Financial Institutions for a customer
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="filters">Attributes to be filtered from the results</param>
+        /// <param name="pageNumber">Cursor that specifies the last resource of the previous page</param>
+        /// <param name="pageSize">Number of items by page</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of financial institution resources</returns>
+        Task<PageBasedXS2ACollection<FinancialInstitutionResponse>> ListForCustomer(CustomerAccessToken token, IEnumerable<Filter> filters = null, long? pageNumber = null, int? pageSize = null, CancellationToken? cancellationToken = null);
     }
 }
