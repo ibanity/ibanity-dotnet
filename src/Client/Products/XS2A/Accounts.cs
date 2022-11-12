b4c49541d05132df701fd5555ae02e367e0c5c68
Stéphane de Wit <contact@stephanedewit.be>
2022-11-12 13:41:25 +0100

    List accounts for financial institution

--- src/Client/Products/XS2A/Accounts.cs
+++ src/Client/Products/XS2A/Accounts.cs
@@ -35,6 +35,10 @@ namespace Ibanity.Apis.Client.Products.XS2A
                 cancellationToken);
 
         /// <inheritdoc />
+        public Task<IbanityCollection<AccountResponse>> ListForFinancialInstitution(CustomerAccessToken token, Guid financialInstitutionId, int? pageLimit = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null) =>
+            InternalCursorBasedList(token, new[] { financialInstitutionId }, null, pageLimit, pageBefore, pageAfter, cancellationToken);
+
+        /// <inheritdoc />
         protected override AccountResponse Map(JsonApi.Data<AccountResponse, AccountMeta, object, object> data)
         {
             var result = base.Map(data);
@@ -66,5 +70,17 @@ namespace Ibanity.Apis.Client.Products.XS2A
         /// <param name="cancellationToken">Allow to cancel a long-running task</param>
         /// <returns>A list of account resources</returns>
         Task<IbanityCollection<AccountResponse>> List(CustomerAccessToken token, int? pageLimit = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null);
+
+        /// <summary>
+        /// List Accounts for Financial Institution
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="financialInstitutionId">Financial institution ID</param>
+        /// <param name="pageLimit">Maximum number (1-100) of resources that might be returned. It is possible that the response contains fewer elements. Defaults to 10</param>
+        /// <param name="pageBefore">Cursor for pagination. Indicates that the API should return the synchronization resources which are immediately before this one in the list (the previous page)</param>
+        /// <param name="pageAfter">Cursor for pagination. Indicates that the API should return the synchronization resources which are immediately after this one in the list (the next page)</param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>A list of account resources</returns>
+        Task<IbanityCollection<AccountResponse>> ListForFinancialInstitution(CustomerAccessToken token, Guid financialInstitutionId, int? pageLimit = null, Guid? pageBefore = null, Guid? pageAfter = null, CancellationToken? cancellationToken = null);
     }
 }
