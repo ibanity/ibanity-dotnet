0f930c2c074b7980135f866398ddd5a67293b4ef
Stéphane de Wit <contact@stephanedewit.be>
2022-11-13 14:38:32 +0100

    Add dummy payment initiation requests service

--- src/Client/Products/XS2A/XS2AClient.cs
+++ src/Client/Products/XS2A/XS2AClient.cs
@@ -30,6 +30,7 @@ namespace Ibanity.Apis.Client.Products.XS2A
             Accounts = new Accounts(apiClient, customerTokenService, UrlPrefix);
             Transactions = new Transactions(apiClient, customerTokenService, UrlPrefix);
             Holdings = new Holdings(apiClient, customerTokenService, UrlPrefix);
+            PaymentInitiationRequests = new PaymentInitiationRequests(apiClient, customerTokenService, UrlPrefix);
         }
 
         /// <inheritdoc />
@@ -61,6 +62,9 @@ namespace Ibanity.Apis.Client.Products.XS2A
 
         /// <inheritdoc />
         public IHoldings Holdings { get; }
+
+        /// <inheritdoc />
+        public IPaymentInitiationRequests PaymentInitiationRequests { get; }
     }
 
     /// <summary>
@@ -131,5 +135,13 @@ namespace Ibanity.Apis.Client.Products.XS2A
         /// <p>The Holding API endpoints are customer specific and therefore can only be accessed by providing the corresponding customer access token.</p>
         /// </summary>
         IHoldings Holdings { get; }
+
+        /// <summary>
+        /// <p>This is an object representing a payment initiation request. When you want to initiate payment from one of your customers, you have to create one to start the authorization flow.</p>
+        /// <p>When creating the payment initiation request, you will receive the redirect link in the response. Your customer should be redirected to this url to begin the authorization process. At the end of the flow, they will be returned to the redirect uri that you defined.</p>
+        /// <p>If the payment initiation is not authorized (for example when the customer cancels the flow), an error query parameter will be added to the redirect uri with the value rejected.</p>
+        /// <p>When authorizing payment initiation from a financial institution user (in the sandbox), you should use 123456 as the digipass response.</p>
+        /// </summary>
+        IPaymentInitiationRequests PaymentInitiationRequests { get; }
     }
 }
