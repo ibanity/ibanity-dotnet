2b8ccd58f326cf361fcb09b252c82707c2bbfe52
Stéphane de Wit <contact@stephanedewit.be>
2022-11-13 22:59:13 +0100

    Allow attributes within relationship

--- src/Client/JsonApi/Relationship.cs
+++ src/Client/JsonApi/Relationship.cs
@@ -7,19 +7,39 @@ namespace Ibanity.Apis.Client.JsonApi
     /// Resource relationship.
     /// </summary>
     [DataContract]
-    public class Relationship
+    public class BaseRelationship
     {
         /// <summary>
         /// Links to other resources.
         /// </summary>
         [DataMember(Name = "links", EmitDefaultValue = false)]
         public Dictionary<string, string> Links { get; set; }
+    }
 
+    /// <summary>
+    /// Resource relationship.
+    /// </summary>
+    [DataContract]
+    public class Relationship : BaseRelationship
+    {
         /// <summary>
         /// Linked resource.
         /// </summary>
         [DataMember(Name = "data", EmitDefaultValue = false)]
         public Data Data { get; set; }
+    }
+
+    /// <summary>
+    /// Resource relationship.
+    /// </summary>
+    [DataContract]
+    public class Relationship<TAttributes> : BaseRelationship
+    {
+        /// <summary>
+        /// Linked resource.
+        /// </summary>
+        [DataMember(Name = "data", EmitDefaultValue = false)]
+        public Data<TAttributes, object, object, object> Data { get; set; }
     }
 
     /// <summary>
