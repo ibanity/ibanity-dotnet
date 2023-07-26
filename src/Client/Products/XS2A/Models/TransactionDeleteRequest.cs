f50c3e35e36d61b08b27be31c96a28730138610d
Christophe Leclef <christophe.leclef@ibanity.com>
2023-07-26 13:34:41 +0200

    Add missing TransactionDeleteRequest object

---
+++ src/Client/Products/XS2A/Models/TransactionDeleteRequest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Ibanity.Apis.Client.Utils;
+
+namespace Ibanity.Apis.Client.Products.XS2A.Models
+{
+    /// <summary>
+    /// <p>This is an object representing a resource transaction delete request.</p>
+    /// <p>The transaction delete request API endpoints can be issued at the level of the application (all customers), a specific customer (all hiis/her accounts), or at account level. The customer access token is therefore only necessary for customer or account.</p>
+    /// </summary>
+    [DataContract]
+    public class TransactionDeleteRequest
+    {
+        /// <summary>
+        /// The requested date before which the transactions should be deleted.
+        /// </summary>
+        /// <value>The requested date before which the transactions should be deleted.</value>
+        [DataMember(Name = "beforeDate", EmitDefaultValue = false)]
+        public string BeforeDate { get; set; }
+    }
+
+    /// <inheritdoc />
+    [DataContract]
+    public class TransactionDeleteRequestRequest : TransactionDeleteRequest
+    {
+        /// <summary>
+        /// The beforeDate should be given in ISO8601 format
+        /// </summary>
+        /// <value>The beforeDate should be given in ISO8601 format</value>
+        [DataMember(Name = "beforeDate", EmitDefaultValue = true)]
+        public string BeforeDate { get; set; }
+    }
+
+    /// <inheritdoc />
+    [DataContract]
+    public class TransactionDeleteRequestResponse : TransactionDeleteRequest, IIdentified<Guid>
+    {
+        /// <inheritdoc />
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The requested date before which the transactions should be deleted.
+        /// </summary>
+        /// <value>The requested date before which the transactions should be deleted.</value>
+        [DataMember(Name = "beforeDate", EmitDefaultValue = false)]
+        public string BeforeDate { get; set; }
+    }
+}
