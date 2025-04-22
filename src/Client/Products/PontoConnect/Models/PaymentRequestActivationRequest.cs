07df0fb21f4d0698551104baaa094f6a11abfad2
Kevin Strijbos <kevin.strijbos@ibanity.com>
2025-04-22 11:39:05 +0200

    Add payment request activation method

---
+++ src/Client/Products/PontoConnect/Models/PaymentRequestActivationRequest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.Serialization;
+using Ibanity.Apis.Client.Utils;
+
+namespace Ibanity.Apis.Client.Products.PontoConnect.Models
+{
+    /// <summary>
+    /// <para>This is an object representing a payment request activation request. If your customer has not yet requested payment request activation for their organization (as indicated by the user info endpoint), you can redirect them to Ponto to submit a request for payment request activation.</para>
+    /// <para>When creating the payment request activation request, you will receive the redirect link in the response. Your customer should be redirected to this url to begin the process. At the end of the flow, they will be returned to the redirect uri that you defined.</para>
+    /// <para>When using this endpoint in the sandbox, the redirection flow will work but the user will not be prompted to request payment activation as this is enabled by default in the sandbox.</para>
+    /// </summary>
+    [DataContract]
+    public class PaymentRequestActivationRequest : Identified<Guid>
+    {
+        /// <summary>
+        /// URI that your user will be redirected to at the end of the authorization flow.
+        /// </summary>
+        [DataMember(Name = "redirectUri", EmitDefaultValue = false)]
+        public Uri Redirect { get; set; }
+    }
+
+    /// <summary>
+    /// URI to redirect to from your customer frontend to conduct the authorization flow.
+    /// </summary>
+    [DataContract]
+    public class PaymentRequestActivationRequestLinks
+    {
+        /// <summary>
+        /// URI to redirect to from your customer frontend to conduct the authorization flow.
+        /// </summary>
+        [DataMember(Name = "redirect", EmitDefaultValue = false)]
+        public string RedirectString { get; set; }
+
+        /// <summary>
+        /// URI to redirect to from your customer frontend to conduct the authorization flow.
+        /// </summary>
+        public Uri Redirect => string.IsNullOrWhiteSpace(RedirectString)
+            ? null
+            : new Uri(RedirectString);
+    }
+}
