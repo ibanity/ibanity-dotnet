9fe36ca8b66b3cc8bbe903275f3639f2631ca8f3
Stéphane de Wit <contact@stephanedewit.be>
2025-09-08 22:24:44 +0200

    Deprecated former 'Status' property

--- src/Client/Products/eInvoicing/Models/PeppolCreditNote.cs
+++ src/Client/Products/eInvoicing/Models/PeppolCreditNote.cs
@@ -37,6 +37,7 @@ namespace Ibanity.Apis.Client.Products.eInvoicing.Models
         /// </summary>
         /// <value>&lt;p&gt;The status of the credit note.&lt;/p&gt;&lt;p&gt;Possible values&lt;/p&gt;&lt;ul&gt;&lt;li&gt;&lt;code&gt;created&lt;/code&gt; The credit note was successfully received by CodaBox and will be processed.&lt;/li&gt;&lt;li&gt;&lt;code&gt;sending&lt;/code&gt; The credit note is valid and will be sent to the customer.&lt;/li&gt;&lt;li&gt;&lt;code&gt;sent&lt;/code&gt; The credit note is sent to the access point of the customer. In this case you will receive a transmissionId.&lt;/li&gt;&lt;li&gt;&lt;code&gt;invalid&lt;/code&gt; The credit note is not valid, you will receive an error object containing a code and a message explaining what went wrong (see  below).&lt;/li&gt;&lt;li&gt;&lt;code&gt;send-error&lt;/code&gt; The credit note could not be sent to the customer, you will receive an error object containing a code and a message explaining what went wrong (see below).    &lt;/li&gt;&lt;/ul&gt;&lt;p&gt;In case of an unspecified error or an issue with the receiving access point, you can try to resend the credit note. CodaBox will not automatically resend the credit note, send-error is a final state of a document.&lt;/p&gt;</value>
         [DataMember(Name = "status", EmitDefaultValue = false)]
+        [Obsolete("Prefer 'TechnicalStatus' property")]
         public string Status { get; set; }
 
         /// <summary>
