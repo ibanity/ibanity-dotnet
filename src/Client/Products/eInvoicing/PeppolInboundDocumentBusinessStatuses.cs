6b3834a35bc83eaa5b7dd7e1e83177cdc18bdf31
Stéphane de Wit <contact@stephanedewit.be>
2025-09-10 20:30:00 +0200

    Add method to create Peppol inbound document business status

--- src/Client/Products/eInvoicing/PeppolInboundDocumentBusinessStatuses.cs
+++ src/Client/Products/eInvoicing/PeppolInboundDocumentBusinessStatuses.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Ibanity.Apis.Client.Http;
 using Ibanity.Apis.Client.Products.eInvoicing.Models;
 
@@ -18,6 +21,24 @@ namespace Ibanity.Apis.Client.Products.eInvoicing
         public PeppolInboundDocumentBusinessStatuses(IApiClient apiClient, IAccessTokenProvider<ClientAccessToken> accessTokenProvider, string urlPrefix) :
             base(apiClient, accessTokenProvider, urlPrefix, new[] { ParentEntityName, EntityName })
         { }
+
+        /// <inheritdoc/>
+        public Task<PeppolInboundDocumentBusinessStatusResponse> Create(ClientAccessToken token, Guid inboundDocumentId, PeppolInboundDocumentBusinessStatus businessStatus, CancellationToken? cancellationToken = null)
+        {
+            if (token is null)
+                throw new ArgumentNullException(nameof(token));
+
+            if (businessStatus is null)
+                throw new ArgumentNullException(nameof(businessStatus));
+
+            var payload = new JsonApi.Data<PeppolInboundDocumentBusinessStatus, object, object, object>
+            {
+                Type = "peppolInboundDocumentBusinessStatus",
+                Attributes = businessStatus
+            };
+
+            return InternalCreate(token, new[] { inboundDocumentId }, payload, null, cancellationToken);
+        }
     }
 
     /// <summary>
@@ -25,5 +46,14 @@ namespace Ibanity.Apis.Client.Products.eInvoicing
     /// </summary>
     public interface IPeppolInboundDocumentBusinessStatuses
     {
+        /// <summary>
+        /// Create Peppol Inbound Document Business Status
+        /// </summary>
+        /// <param name="token">Authentication token</param>
+        /// <param name="inboundDocumentId">Inbound document ID</param>
+        /// <param name="businessStatus"></param>
+        /// <param name="cancellationToken">Allow to cancel a long-running task</param>
+        /// <returns>Returns a Peppol Credit Note resource</returns>
+        Task<PeppolInboundDocumentBusinessStatusResponse> Create(ClientAccessToken token, Guid inboundDocumentId, PeppolInboundDocumentBusinessStatus businessStatus, CancellationToken? cancellationToken = null);
     }
 }
